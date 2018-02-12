720b972cd233f3e97530de05c8b1a8adcd37b658
wdxxl <wdxxlanswer@gmail.com>
2018-02-12 15:21:34 +0800

    add maximum size of lucene filed and highlighter size.

--- lucene_3_5/src/main/java/com/wdxxl/lucene/highlighter/HighlighterMaximum.java
+++ lucene_3_5/src/main/java/com/wdxxl/lucene/highlighter/HighlighterMaximum.java
@@ -3,7 +3,6 @@ package com.wdxxl.lucene.highlighter;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,16 +23,13 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.highlight.Highlighter;
 import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
 import org.apache.lucene.search.highlight.NullFragmenter;
 import org.apache.lucene.search.highlight.QueryScorer;
-import org.apache.lucene.search.highlight.SimpleFragmenter;
 import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.LockObtainFailedException;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
@@ -42,60 +38,21 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-public class HighlighterCountDemo {
+public class HighlighterMaximum {
 	static Directory dir = new RAMDirectory();
-
 	static Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_35);
-	static String index_folder = "./lib/Highlighter_count/";
+	static String[] bookNames = { "./lib/highlighter/FALTCHAP-27.json" };
 
 	public static void main(String[] args) throws IOException, ParseException, InvalidTokenOffsetsException {
-		dir = FSDirectory.open(new File(System.getProperty("user.dir")+ "/lib/lucene35/highlightcount"));
-		Date start = new Date();
 		index();
-		Date end = new Date();
-		System.out.println("It tooks : " + ((end.getTime() - start.getTime())) +" for Index.");
-		System.out.println("--------------------Query Top Docs Without Display---------------------------");
-		start = new Date();
-		TopDocs topDocs = searcher("bookName", "from");
-		end = new Date();
-		System.out.println("It tooks : " + ((end.getTime() - start.getTime())) +" for Query Top Docs.");
-		System.out.println("Matched TopDocs: "+topDocs.totalHits);
-
-		System.out.println("--------------------Query Top Docs with Display---------------------------");
-		start = new Date();
-		topDocs = searcher("bookName", "from");
-		display(topDocs);
-		end = new Date();
-		System.out.println("It tooks : " + ((end.getTime() - start.getTime())) +" for Query Top Docs.");
-
-		System.out.println("--------------------TermQuery---------------------------");
-		Query query = new TermQuery(new Term("bookName", "from"));
-		start = new Date();
-		highLightDisplay(topDocs, query, false);
-		end = new Date();
-		System.out.println("It tooks : " + ((end.getTime() - start.getTime())) +" for TermQuery.");
-
-		System.out.println("--------------------PhraseQuery---------------------------");
-		PhraseQuery phraseQuery = new PhraseQuery();
-		phraseQuery.setSlop(3);
-		phraseQuery.add(new Term("bookName", "partially"));
-		phraseQuery.add(new Term("bookName", "boxed"));
-		start = new Date();
-		highLightDisplay(topDocs, phraseQuery, false);
-		end = new Date();
-		System.out.println("It tooks : " + ((end.getTime() - start.getTime())) +" for PhraseQuery.");
-
-		System.out.println("--------------------PhraseQuery---------------------------");
-		phraseQuery = new PhraseQuery();
-		phraseQuery.setSlop(3);
-		phraseQuery.add(new Term("bookName", "sec"));
-		phraseQuery.add(new Term("bookName", "from"));
-		phraseQuery.add(new Term("bookName", "standard"));
-		start = new Date();
-		highLightDisplay(topDocs, phraseQuery, false);
-		end = new Date();
-		System.out.println("It tooks : " + ((end.getTime() - start.getTime())) +" for PhraseQuery.");
-
+		System.out.println("--------------------PhraseQuery-------Exact Match 1--------------------");
+		TopDocs topDocs = searcher("bookName", "partially");// 27: partially
+															// 28:fuel
+		PhraseQuery query = new PhraseQuery();
+		query.setSlop(3); // 允许空一个词
+		query.add(new Term("bookName", "partially")); // 27: partially 28:fuel
+		query.add(new Term("bookName", "boxed")); // 27: boxed 28:leak
+		highLightDisplay(topDocs, query);
 	}
 
 	public static TopDocs searcher(String fieldName, String keyWords)
@@ -109,20 +66,7 @@ public class HighlighterCountDemo {
 		return topDocs;
 	}
 
-	// 把查询到的图书进行输出
-	public static void display(TopDocs topDocs) throws CorruptIndexException, IOException {
-		IndexReader reader = IndexReader.open(dir);
-		IndexSearcher searcher = new IndexSearcher(reader);
-		ScoreDoc[] scoreDoc = topDocs.scoreDocs;
-		for (int i = 0; i < scoreDoc.length; i++) {
-			Document doc = searcher.doc(scoreDoc[i].doc);
-			doc.get("bookName");
-			//System.out.println(doc.get("bookName"));
-		}
-		searcher.close();
-	}
-
-	public static void highLightDisplay(TopDocs topDocs, Query query, boolean display)
+	public static void highLightDisplay(TopDocs topDocs, Query query)
 			throws CorruptIndexException, IOException, ParseException, InvalidTokenOffsetsException {
 		IndexReader reader = IndexReader.open(dir);
 		IndexSearcher searcher = new IndexSearcher(reader);
@@ -130,38 +74,38 @@ public class HighlighterCountDemo {
 
 		SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<font color='red'>", "</font>");
 		Highlighter highlighter = new Highlighter(simpleHTMLFormatter, new QueryScorer(query));
-		highlighter.setMaxDocCharsToAnalyze(2 * 1024 * 1024);
+		highlighter.setMaxDocCharsToAnalyze(1024 * 1024 * 1024); // 2,097,152
 		highlighter.setTextFragmenter(new NullFragmenter());
-		//highlighter.setTextFragmenter(new SimpleFragmenter(200));
 		for (int i = 0; i < scoreDoc.length; i++) {
 			Document doc = searcher.doc(scoreDoc[i].doc);
-			String fileName = doc.get("fileName");
 			String text = doc.get("bookName");
 			TokenStream tokenStream = analyzer.tokenStream("bookName", new StringReader(text));
 			String highLightText = highlighter.getBestFragment(tokenStream, text);
+			System.out.println("highLightText Length:" + highLightText.length());
 			if (highLightText != "" && highLightText != null) {
-				System.out.print("fileName: " + fileName + ", Count:" + countHighlighter(highLightText) +", ");
-				if(display){
-					System.out.println();
-					System.out.println(highLightText);
-				}
+				countHighlighter(highLightText);
 			}
 		}
-		System.out.println();
 		searcher.close();
 	}
 
 	public static void index() throws CorruptIndexException, LockObtainFailedException, IOException {
 		IndexWriter index = new IndexWriter(dir, new IndexWriterConfig(Version.LUCENE_35, analyzer));
-		File folder = new File(index_folder);
-		for (final File fileEntry : folder.listFiles()) {
-			if (!fileEntry.isDirectory()) {
-				Document doc = new Document();
-				doc.add(new Field("fileName", fileEntry.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-				doc.add(new Field("bookName", getContentFromJson(fileEntry.getAbsolutePath()), Field.Store.YES,
-						Field.Index.ANALYZED));
-				index.addDocument(doc);
-			}
+		for (int i = 0; i < bookNames.length; i++) {
+			Document doc = new Document();
+			StringBuilder sb = new StringBuilder();
+			sb.append(getContentFromJson(bookNames[i]))
+			.append(getContentFromJson(bookNames[i]))
+			.append(getContentFromJson(bookNames[i]))
+			.append(getContentFromJson(bookNames[i]))
+			.append(getContentFromJson(bookNames[i]))
+			.append(getContentFromJson(bookNames[i]))
+			.append(getContentFromJson(bookNames[i]))
+			.append(getContentFromJson(bookNames[i]))
+			.append(getContentFromJson(bookNames[i]));
+			System.out.println("Index Length:" + sb.toString().length());
+			doc.add(new Field("bookName", sb.toString(), Field.Store.YES, Field.Index.ANALYZED));
+			index.addDocument(doc);
 		}
 		index.close();
 	}
@@ -182,6 +126,13 @@ public class HighlighterCountDemo {
 		while (matcher.find()) {
 			count++;
 		}
+		System.out.println(count);
 		return count;
 	}
+
+	public static void writeToHTMLFiles(String fileName, String data) throws IOException {
+		File file = new File(fileName);
+		FileUtils.writeStringToFile(file, data, "UTF-8", false);
+	}
+
 }
