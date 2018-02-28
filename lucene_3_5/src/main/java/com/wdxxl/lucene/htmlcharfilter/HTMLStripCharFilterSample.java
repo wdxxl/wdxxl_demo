5b9f970282e585ab0e5441a1e50822d02a49ea9d
wdxxl <wdxxlanswer@gmail.com>
2018-02-28 16:48:35 +0800

    add HTMLStripCharFilter for filter html tag - base on lucene-analyzers 3.6.0

---
+++ lucene_3_5/src/main/java/com/wdxxl/lucene/htmlcharfilter/HTMLStripCharFilterSample.java
@@ -0,0 +1,45 @@
+package com.wdxxl.lucene.htmlcharfilter;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharReader;
+import org.apache.lucene.analysis.MappingCharFilter;
+import org.apache.lucene.analysis.NormalizeCharMap;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.util.Version;
+
+// https://github.com/mwsoft/sample/blob/master/solr-filter-sample/src/jp/mwsoft/sample/lucene/filter/HTMLStripCharFilterSample.java
+public class HTMLStripCharFilterSample {
+
+    public static void main(String[] args) throws Exception {
+        MyAnalyzer analyzer = new MyAnalyzer();
+
+        String str = "<html><head><title>title1 element1<\\/title><script>script2 element2</script></head><body>body3 element3</body></html>";
+
+        Reader reader = new StringReader(str);
+        TokenStream stream = analyzer.tokenStream("", reader);
+
+        while (stream.incrementToken()) {
+            CharTermAttribute term = stream.getAttribute(CharTermAttribute.class);
+            System.out.print(term.toString() + "\t");
+        }
+        // => title element body    element
+    }
+
+    static class MyAnalyzer extends Analyzer {
+    	final static NormalizeCharMap map = new NormalizeCharMap();
+    	static {
+    		map.add( "\\/", "/");
+    	}
+        public final TokenStream tokenStream(String fieldName, Reader reader) {
+        	reader = new MappingCharFilter(map, CharReader.get(reader));
+            reader = new HTMLStripCharFilter(CharReader.get(reader));
+            TokenStream result = new StandardTokenizer(Version.LUCENE_35, reader);
+            return result;
+        }
+    }
+}
